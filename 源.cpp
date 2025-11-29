1d6ffad2bd19eabe05a9951b4a7b21eb21d7fe53
shidage88 <shiba0923s@qq.com>
2025-11-29 15:21:06 +0800

    Add files via upload

--- 源.cpp
+++ 源.cpp
@@ -1,13 +1,28 @@
-#include<iostream>
-#include<iomanip>
+#include <iostream>
+#include <<<iomanip> 
 using namespace std;
-int main()
-{
-	cout << fixed << setprecision(2);
-	double C;
-	double F = 0;
-	C = (F - 32) / 1.8;
-	cout << "请输入华氏温度" << endl;
-	cin >> F;
-	cout <<"摄氏温度为"<< C << endl;
+
+int main() {
+    const double apple_price = 0.8; 
+    int day = 1;                    
+    int buy_count = 2;              
+    double total_cost = buy_count * apple_price; 
+
+    
+    while (true) {
+        int next_count = buy_count * 2; 
+        if (buy_count + next_count > 100) {
+            break;
+        }
+        buy_count = next_count;
+        total_cost += buy_count * apple_price;
+        day++;
+    }
+
+ 
+    double avg_cost = total_cost / day;
+    cout << "总购买天数：" << day << endl;
+    cout << "每天平均花费：" << fixed << setprecision(2) << avg_cost << "元" << endl;
+
+    return 0;
 }
